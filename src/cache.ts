12768a6249d20acaaed283d001cc9470b841a5b2
luke-h1 <luke.howsam@yahoo.com>
2021-06-27 22:30:20 +0100

    redis: fetch, get, set, del fns

--- src/cache.ts
+++ src/cache.ts
@@ -1,3 +1,36 @@
-// import { redis } from "./redis";
+import { redis } from './redis';
 
-export default {};
+const fetch = async <T>(key: string, fetcher: () => T, expires: number) => {
+  /**
+   * If it's in the cache return from redis
+   * else make an API call and set result in redis
+   */
+  const isItInTheCache = await get<T>(key);
+  if (isItInTheCache !== null) return isItInTheCache;
+  return set(key, fetcher, expires);
+};
+
+const get = async <T>(key: string): Promise<T | null> => {
+  const value = await redis.get(key);
+  if (value === null) return null;
+  return JSON.parse(value);
+};
+
+const set = async <T>(key: string, fetcher: () => T, expires: number) => {
+  const value = await fetcher();
+  // eslint-disable-next-line no-console
+  console.log(value);
+  await redis.set(key, JSON.stringify(value), 'EX', expires);
+  return value;
+};
+
+const del = async (key: string) => {
+  await redis.del(key);
+};
+
+export default {
+  fetch,
+  get,
+  set,
+  del,
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
-// import { GetServerSideProps } from 'next';
-// import cache from '@src/cache';
+import { GetServerSideProps } from 'next';
+import cache from '@src/cache';
 
 interface IProps {
   coupon: string | null;
+}
+
+interface IPPPData {
+  ppp: {
+    pppConversionFactor: number;
+  };
 }
 
 const Home = ({ coupon }: IProps) => {
@@ -14,10 +20,41 @@ const Home = ({ coupon }: IProps) => {
           {coupon}
         </h1>
       ) : (
-        <h1>Sorry no coupon</h1>
+        <h1>Sorry...no coupon</h1>
       )}
     </div>
   );
 };
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const country = 'CO';
+
+  const fetcher = async () => {
+    const url = `https://api.purchasing-power-parity.com/?target=${country}`;
+    const resp = await fetch(url);
+    const data: IPPPData = await resp.json();
+    console.log(new Date());
+    let coupon: string | null = null;
+    if (data.ppp.pppConversionFactor < 0.25) {
+      coupon = 'PPP75';
+    } else if (data.ppp.pppConversionFactor < 0.5) {
+      coupon = 'PPP50';
+    } else if (data.ppp.pppConversionFactor < 0.75) {
+      coupon = 'PPP25';
+    }
+    return coupon;
+  };
+  // cache.del(`ppp:${country}`)
+
+  const cachedCoupon = await cache.fetch<Promise<string | null>>(
+    `ppp:${country}`,
+    fetcher,
+    60 * 60,
+  ); // cache for 1hr
+
+  return {
+    props: { coupon: cachedCoupon },
+  };
+};
 // https://api.purchasing-power-parity.com/?target=
